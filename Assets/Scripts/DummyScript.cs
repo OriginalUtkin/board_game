91fa58923ce3aa9591c7b42d418583ed18110ff1
Konstantin Kalentev <konstantin.kalentev@kiwi.com>
2020-09-05 12:56:37 +0200

    add empty.md files

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -7,7 +7,15 @@ public class DummyScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< HEAD
         Debug.Log("DummyScript started");
+=======
+        int x = 2;
+        int y = x*2;
+        Debug.Log("DummyScript started");
+        Debug.Log(string.Format("x: {0} y: {1}", x, y));
+
+>>>>>>> eeb9c6c... add empty.md files
     }
 
     // Update is called once per frame
