d27f09af84ad261fd1d71f97f527e763930657f3
Kirill <utkinkir@gmail.com>
2020-09-05 15:37:44 +0200

    Define creature card class

---
+++ Assets/Scripts/Card/CreatureCard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+   private Sprite cardFace;
+   private Sprite cardBack;
+   private int health;
+   private int attack;
+   private int armor;
+
+   public Card(Sprite cardFace, Sprite cardBack, int health, int attack, int armor){
+       this.cardFace = cardFace;
+       this.health = health;
+       this.attack = attack;
+       this.armor = armor;
+   }
+
+   public int Health{
+       get { return this.health; }
+       set { this.health = value; }
+   }
+
+   public int Attack{
+       get { return this.attack; }
+       set { this.attack = value; }
+   }
+
+   public int Armor{
+       get { return this.armor; }
+       set { this.armor = value; }
+   }
+}
