a68f712ad3e5c9905cf4d18348ca1ae9f039cf87
Konstantin Kalentev <konstantin.kalentev@kiwi.com>
2020-09-05 17:15:43 +0200

    Add simple card play

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject playerHand; 
+    public GameObject playSlot;
+
+    public GameObject simpleCardPrefab;
+    public GameObject[] facesPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateNewCard();
+    }
+
+    private void CreateNewCard() {
+        Debug.Log("creating new card");
+        GameObject newCard = Instantiate(simpleCardPrefab, playerHand.transform);
+        newCard.GetComponent<SimpleCard>().cardFaceTemplate = facesPrefabs[0];
+        newCard.GetComponent<SimpleCard>().mainScript = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayCard(SimpleCard card) {
+        Debug.Log("playing card");
+        Debug.Log(card);
+        card.transform.SetParent(playSlot.transform);
+        card.transform.position = Vector3.zero;
+    }
+}
