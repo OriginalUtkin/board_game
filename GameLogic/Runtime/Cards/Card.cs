1f16bba4276c6220d69e2a805a08718985ca948f
Konstantin Kalentev <konstantin.kalentev@kiwi.com>
2020-09-06 15:35:08 +0200

    Add Card and CardBuilder classes

---
+++ GameLogic/Runtime/Cards/Card.cs
@@ -0,0 +1,37 @@
+﻿namespace BoardGame.Cards {
+
+    public class Card
+    {
+        private string name;
+        private int health;
+        private int attack;
+
+        public Card(string name, int health, int attack) {
+            this.name = name;
+            this.health = health;
+            this.attack = attack;
+        }
+
+        public Card(Card previous) {
+            this.name = previous.Name;
+            this.health = previous.Health;
+            this.attack = previous.Attack;
+        }
+
+        public string Name{
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Health{
+            get { return this.health; }
+            set { this.health = value; }
+        }
+
+        public int Attack{
+            get { return this.attack; }
+            set { this.attack = value; }
+        }
+
+    }
+
+}
---
+++ GameLogic/Runtime/Cards/CardBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+
+namespace BoardGame.Cards {
+
+    public class CardBuilder
+    {
+        static Dictionary<string, Card> cardTemplates;
+
+        static CardBuilder() {
+            cardTemplates = new Dictionary<string, Card>();
+            cardTemplates.Add("gnome", new Card("gnome", 1, 5));
+            cardTemplates.Add("goblin", new Card("goblin", 2, 4));
+            cardTemplates.Add("demon", new Card("demon", 3, 3));
+            cardTemplates.Add("elf", new Card("elf", 4, 2));
+        }
+
+        public Card create(string name) {
+            return new Card(cardTemplates[name]);
+        }
+    }
+
+
+}
