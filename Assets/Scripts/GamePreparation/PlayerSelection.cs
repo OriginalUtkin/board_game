4900b2581579a67ca2540653e1e7d013f9595036
Konstantin Kalentev <konstantin.kalentev@kiwi.com>
2020-09-06 18:32:29 +0200

    Draw player selection panel

---
+++ Assets/Scripts/GamePreparation/PlayerSelection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BoardGame.Cards;
+public class PlayerSelection : MonoBehaviour
+{
+    public CardCreator cardCreator;
+    public float yOffset = 1.72f;
+
+
+    public void SyncWithCardList(List<Card> cards) {
+        for (int i = 0; i < cards.Count; i++) {
+            GameObject cardObj = cardCreator.createCard(cards[i], transform);
+            cardObj.transform.position += yOffset * i * Vector3.down;
+        }
+    }
+}
