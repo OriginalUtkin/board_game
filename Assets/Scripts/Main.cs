748d1bb140c2aeff6642cc816f4b8b8c4c5cc9cb
Kirill <utkinkir@gmail.com>
2020-09-06 20:03:58 +0200

    Add HandPosition handler

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -10,6 +10,10 @@ public class Main : MonoBehaviour
     public GameObject simpleCardPrefab;
     public GameObject[] facesPrefabs;
 
+    private Vector3 HandPosition{
+        get { return this.playerHand.transform.position; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +23,15 @@ public class Main : MonoBehaviour
     private void fillPlayerHand(int hand_size) {
          Debug.Log("filling player hand with cards");
 
-         Vector3 handPosition = this.playerHand.transform.position;
-
          int offset = 2;
 
          int left_side = hand_size / 2;
          int right_side = hand_size - left_side;
 
-         Vector3 cardPosition = new Vector3(x: handPosition.x - left_side * 1.5f, y: handPosition.y, z: handPosition.z);
+         this.playerHand.transform.position = new Vector3(x: this.HandPosition.x - left_side * 1.5f, y: this.HandPosition.y, z: this.HandPosition.z);
+
+
+         Vector3 cardPosition = new Vector3(x: this.HandPosition.x , y: this.HandPosition.y, z: this.HandPosition.z);
 
          for (int card_counter = 0; card_counter < hand_size; card_counter++){
              GameObject newCard = Instantiate(original: simpleCardPrefab, position: cardPosition, rotation: Quaternion.identity, parent: playerHand.transform);
