9a7bb81f1cccf4cec22821cbd232db43467be76f
Kirill <utkinkir@gmail.com>
2020-09-06 17:55:10 +0200

    Fill player hand with 3 cards

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -13,14 +13,24 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateNewCard();
+        this.fillPlayerHand(hand_size: 3);
     }
 
-    private void CreateNewCard() {
-        Debug.Log("creating new card");
+    private void fillPlayerHand(int hand_size) {
+        Debug.Log("filling player hand with cards");
 
-        GameObject newCard = Instantiate(simpleCardPrefab, playerHand.transform);
-        newCard.GetComponent<SimpleCard>().Setup(facesPrefabs[0], this);
+        Vector3 handPosition = this.playerHand.transform.position;
+        Vector3 cardPosition = new Vector3(x: handPosition.x, y: handPosition.y, z: handPosition.z);
+
+        int offset = 2;
+
+        for (int card_counter = 0; card_counter < hand_size; card_counter++){
+            GameObject newCard = Instantiate(original: simpleCardPrefab, position: cardPosition, rotation: Quaternion.identity, parent: playerHand.transform);
+            newCard.GetComponent<SimpleCard>().Setup(facesPrefabs[0], this);
+
+            cardPosition.x += offset;
+        }
+
     }
 
     // Update is called once per frame
