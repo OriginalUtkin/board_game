1e511e78091b2466ff86fe40a1a00aa72f7aa2e2
Kirill <utkinkir@gmail.com>
2020-09-13 18:21:33 +0200

    Introduce battlefield

---
+++ Assets/Scripts/GameProcess/PlayerBattlefield.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattlefield : MonoBehaviour
+{
+    public GameObject[] playerSlots;
+
+    public int NextCardPosition{ get; set;} = 0;
+
+    public Action<PlayerBattlefield> ClickAction{ get; set; }
+    
+    private void OnMouseDown() {
+        Debug.Log("Player battlefield is clicked");
+        this.ClickAction(this);
+    }
+
+}
