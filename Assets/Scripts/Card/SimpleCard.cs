818c3af419bec4796c8155f03e895a6f1e965183
Kirill <utkinkir@gmail.com>
2020-12-29 18:38:17 +0100

    Define mouse event methods

--- Assets/Scripts/Card/SimpleCard.cs
+++ Assets/Scripts/Card/SimpleCard.cs
@@ -8,10 +8,11 @@ public class SimpleCard : MonoBehaviour
     GameObject cardFace;
     Action<SimpleCard> clickAction;
     public Guid cardGuid;
+    public bool isSelected { get; private set; } = false;
 
     public void Setup(GameObject _cardFaceTemplate, Action<SimpleCard> _clickAction, Guid _cardGuid)
     {
-        cardFace = Instantiate(_cardFaceTemplate, transform);
+        cardFace = Instantiate(_cardFaceTemplate, this.transform);
         clickAction = _clickAction;
         cardGuid = _cardGuid;
     }
@@ -23,6 +24,23 @@ public class SimpleCard : MonoBehaviour
 
     private void OnMouseDown()
     {
+        this.isSelected = true;
         clickAction(this);
     }
+
+    private void OnMouseUp()
+    {
+        this.isSelected = false;
+        // Move card back to the hand
+    }
+
+    private void Update()
+    {
+        if (this.isSelected)
+        {
+            Debug.Log("Mouse button is clicked; Updating card position");
+        }
+    }
+
+
 }
