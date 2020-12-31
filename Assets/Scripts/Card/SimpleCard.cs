9d25d99b800ebe66697c9bbae671bca10f529d22
Kirill <utkinkir@gmail.com>
2020-12-31 13:19:23 +0100

    Implement drag and drop

--- Assets/Scripts/Card/SimpleCard.cs
+++ Assets/Scripts/Card/SimpleCard.cs
@@ -2,45 +2,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SimpleCard : MonoBehaviour
 {
     GameObject cardFace;
-    Action<SimpleCard> clickAction;
     public Guid cardGuid;
     public bool isSelected { get; private set; } = false;
+    public bool isPlacedOnBattlefield { get; private set; } = false;
 
-    public void Setup(GameObject _cardFaceTemplate, Action<SimpleCard> _clickAction, Guid _cardGuid)
+    public void Setup(GameObject _cardFaceTemplate, Guid _cardGuid)
     {
         cardFace = Instantiate(_cardFaceTemplate, this.transform);
-        clickAction = _clickAction;
         cardGuid = _cardGuid;
-    }
-
-    public void UpdateClickAction(Action<SimpleCard> _clickAction)
-    {
-        clickAction = _clickAction;
     }
 
     private void OnMouseDown()
     {
         this.isSelected = true;
-        clickAction(this);
     }
 
     private void OnMouseUp()
     {
+        Debug.Log("Mouse button is released; Do not move card " + this.cardGuid);
+
         this.isSelected = false;
-        // Move card back to the hand
+        IInteractable? collisionObject = this.GetColissionObject();
+
+        if (collisionObject == null)
+            this.PutCardToHand();
+        else
+            collisionObject.RecieveObject(this);
     }
 
     private void Update()
     {
         if (this.isSelected)
         {
-            Debug.Log("Mouse button is clicked; Updating card position");
+            Debug.Log("Mouse button is clicked; Updating card position " + this.cardGuid);
+
+            Vector3 coursorePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.position = new Vector3(coursorePosition.x, coursorePosition.y, -5.0f);
+
+            Debug.Log("Mouse position " + Input.mousePosition + " Card position " + this.transform.position);
         }
     }
 
+    private IInteractable? GetColissionObject()
+    {
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(
+            this.transform.position,
+            transform.TransformDirection(Vector3.forward),
+            out hit,
+            Mathf.Infinity,
+            Physics.DefaultRaycastLayers
+        );
 
+        Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.forward) * 20.0f, isHit ? Color.yellow : Color.red);
+
+        if (hit.collider == null)
+            return null;
+
+        IInteractable collisionObject = (IInteractable)hit.collider.GetComponent(hit.collider.name);
+
+        return collisionObject;
+    }
+
+    private void PutCardToHand()
+    {
+
+    }
 }
