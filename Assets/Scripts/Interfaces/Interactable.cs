bcffbd9c8da3867f5ab04e97afa6c3a3e0f96719
Kirill <utkinkir@gmail.com>
2020-12-31 13:15:43 +0100

    Define interface for communication between objects on scene

---
+++ Assets/Scripts/Interfaces/Interactable.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BoardGame.Cards;
+using UnityEngine;
+interface IInteractable{
+    void RecieveObject(MonoBehaviour obj);
+}
