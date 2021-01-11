4066d5a77d9c0eb3222a05436a1d8b8026230990
Kirill <utkinkir@gmail.com>
2021-01-11 19:12:42 +0100

    Adjust player battlefield: v1

--- Assets/Scripts/GameProcess/PlayerBattlefield.cs
+++ Assets/Scripts/GameProcess/PlayerBattlefield.cs
@@ -7,9 +7,9 @@ public class PlayerBattlefield : MonoBehaviour, IInteractable
 {
     public GameObject[] playerSlots;
 
-    public int NextCardPosition { get; set; } = 0;
+    private const int Capacity = 5;
 
-    public Action<PlayerBattlefield> ClickAction { get; set; }
+    private int AvailableSpots { get; set; } = PlayerBattlefield.Capacity;
 
     private void OnMouseDown()
     {
@@ -19,6 +19,45 @@ public class PlayerBattlefield : MonoBehaviour, IInteractable
     public void ReceiveObject(MonoBehaviour obj)
     {
         Debug.Log("Object is received");
+        int cardPosition;
+
+        if (this.AvailableSpots == PlayerBattlefield.Capacity)
+            cardPosition = PlayerBattlefield.Capacity / 2;
+        else
+            cardPosition = this.CalculateCardPosition();
+
+        obj.transform.position = this.playerSlots[cardPosition].transform.position;
+
+        this.AvailableSpots -= 1;
+    }
+
+    private int CalculateCardPosition()
+    {
+        Debug.Log("Calculating spot coordinates");
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        double closestDistance = Vector3.Distance(this.playerSlots[0].transform.position, cursorPosition);
+        int closestIndex = 0;
+
+        for (int i = 1; i < this.playerSlots.Length; i++)
+        {
+            double distance = Vector3.Distance(this.playerSlots[i].transform.position, cursorPosition);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestIndex = i;
+            }
+        }
+
+        Debug.Log("Closest slot is " + closestIndex);
+
+        return closestIndex;
+    }
+
+    public bool IsReceivable()
+    {
+        return this.AvailableSpots > 0;
     }
 
 }
